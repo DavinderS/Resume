169b9c9db1050ad52953e0b683b96f830468e5a6
DavinderS <daveshinh@gmail.com>
2015-09-29 22:33:21 -0400

    omg

--- Parallax/Parallax.js
+++ Parallax/Parallax.js
@@ -1,30 +1,43 @@
 $(window).on('beforeunload', function() {
-	// Always start from scroll = 0
+    // Always start from scroll = 0 (Browsers save scroll location in certain cases)
+    // can't use it on ready because it scrolls after it's ready
     $(window).scrollTop(0);
 });
-var moving = false;
-var target = 0;
-$(document).ready(function(){
-	document.onmousewheel = function(e) {
-		if (moving == false)
-		{
-			if (e.deltaY > 0)
-			{
-				target = window.pageYOffset + window.innerHeight
-			}
-			if (e.deltaY < 0)
-			{
-				target = window.pageYOffset - Math.max(window.innerHeight, 0)
-			}
-			update(window.pageYOffset, target)
-		}
-	}
-	update = function(initialPoint, target) {
-		moving = true
-		$('html, body').animate({
-    	scrollTop: target
- 	}, 1000, function() {
- 		moving = false
- 	});
-	}
+
+$(document).ready(function() {
+	var moving = false;
+	var target = 0;
+	var page = 0
+    document.onmousewheel = function(e) {
+        if (moving == false) {
+            if (e.deltaY > 0) {
+                target = window.pageYOffset + window.innerHeight
+                if (target < document.body.scrollHeight) {
+                    page++;
+                    moving = true
+                }
+            } else if (e.deltaY < 0) {
+                target = window.pageYOffset - window.innerHeight
+                if (target >= 0) {
+                    page--;
+                    moving = true
+                }
+            }
+            if (moving) {
+                update(target)
+            }
+        }
+    }
+    update = function(target) {
+        $('html, body').animate({
+            scrollTop: target
+        }, 1000, function() {
+            moving = false
+
+        });
+    }
+    window.onresize = function(e)
+    {
+    	window.scrollTo(0, window.innerHeight * page)
+    }
 });
