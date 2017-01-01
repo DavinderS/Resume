6c9231d253432ecddc3d1f950b083e74f9f572fc
DavinderS <daveshinh@gmail.com>
2016-12-31 23:06:23 -0500

    Stuff

--- Parallax/Parallax.js
+++ Parallax/Parallax.js
@@ -1,15 +1,41 @@
-$(document).ready(function() {
+$(window).ready(function() {
+    var test = true;
+
     resizeText = function() {
-        console.log(window.innerWidth);
-        if (window.innerWidth < 500) {
+        if (window.innerWidth < 650) {
             $("h1").addClass("h1Mobile");
-            $("p").addClass("pMobile");
+            $("h2").addClass("h2Mobile");
         } else {
             $("h1").removeClass("h1Mobile");
-            $("p").removeClass("pMobile");
+            $("h2").removeClass("h2Mobile");
         }
     }
+
+
+
+    hideLoader = function() {
+        $(".loadingPanel").fadeOut(750, showContent);
+    }
+    showContent = function() {
+        $(".content").fadeIn(750, showButtons);
+    }
+    showButtons = function() {
+    /*    $(".button").animate({
+            top: "20px"
+        },
+        750);*/
+    }
+
+
     resizeText();
     $(window).resize(resizeText);
+    if (test == false)
+    {
+    setTimeout(hideLoader,3000);
+} else {
+        $(".loadingPanel").hide();
+        $(".content").show();
+
+}
 
 })
